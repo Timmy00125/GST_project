4424a217695071a79dc036b463797061bbc56ab0
Timmy <timilehinoluwaseyi07@gmail.com>
2025-01-24 00:27:50 +0100

    changed to postgres

--- vema/vema/settings.py
+++ vema/vema/settings.py
@@ -103,13 +103,24 @@ AUTHENTICATION_BACKENDS = [
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
+
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "gst",  # Database name
+        "USER": "Timmy",  # PostgreSQL username
+        "PASSWORD": "Letmein",  # PostgreSQL password
+        "HOST": "localhost",  # Set to 'db' if using Docker
+        "PORT": "5432",  # Default PostgreSQL port
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
